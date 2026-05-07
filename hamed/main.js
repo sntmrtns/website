edc936690f67aa6852b526be059168e7dbc30bf7
sntmrtns <150751970+sntmrtns@users.noreply.github.com>
2026-05-06 23:30:37 -0400

    Revert lazy loading and restore original fade-in

--- hamed/main.js
+++ hamed/main.js
@@ -48,8 +48,4 @@ function showSection(name) {
 
   window.scrollTo(0, 0);
 
-  document.getElementById("section-" + name).querySelectorAll("iframe[data-src]").forEach(function(iframe) {
-    iframe.src = iframe.getAttribute("data-src");
-    iframe.removeAttribute("data-src");
-  });
 }
